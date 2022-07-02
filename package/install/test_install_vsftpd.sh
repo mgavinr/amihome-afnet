88814996340c3a11fa02b2ab59d1143d01f40d96
Gavin Rogers <mgavinr@gmail.com>
2022-07-03 00:28:01 +0100

    PD-None: add apt and yum...............

--- package/install/test_install_vsftpd.sh
+++ package/install/test_install_vsftpd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export AFHOME=`dirname $0`; until ls ${AFHOME}/etc/afnet.conf > /dev/null 2>&1; do cd ${AFHOME}/..; export AFHOME=`pwd`; done; echo "Home Directory: `pwd`"; source ${AFHOME}/etc/afnet.conf
 #
-sudo yum install vsftpd
+which yum && sudo yum install vsftpd
+which apt && sudo apt-get install vsftpd
 systemctl start vsftpd
